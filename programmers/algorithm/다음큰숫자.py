014906632df2ff802b351240cfbd1637a4ca6486
fbgjung <fbgjung@gmail.com>
2024-02-05 16:51:38 +0900

    [프로그래머스][Lv2] 다음 큰 숫자

---
+++ programmers/algorithm/다음큰숫자.py
@@ -0,0 +1,12 @@
+def solution(n):
+    answer = n+1
+    
+    while True:
+        if bin(answer).count('1') == bin(n).count('1'):
+            break
+        else:
+            answer += 1 
+
+    return answer
+            
+print(solution(78))
