18cdb2e769aa71d525c59dcbe24a36012c735043
fbgjung <fbgjung@gmail.com>
2024-02-10 20:03:28 +0900

    modified: [백준][브론즈1][구현] 세로읽기

--- baekjoon/세로읽기.py
+++ baekjoon/세로읽기.py
@@ -4,9 +4,7 @@ for _ in range(5):
     S = input()
     data.append(S)
 
-
 for j in range(15):
     for i in range(5):
-        # if j < len(data[i]):
-        print(data[i][j], end='')
-        
+        if j < len(data[i]):
+            print(data[i][j], end='')
