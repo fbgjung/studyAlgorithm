590145663c67291b3bebbf287178eaa523e94ab3
jjungeeu <fbgjung@gmail.com>
2024-02-20 21:21:41 +0900

    #10 [백준][실버4][이분탐색] 수 찾기

--- baekjoon/class2/수찾기.py
+++ baekjoon/class2/수찾기.py
@@ -6,8 +6,21 @@ A = list(map(int, sys.stdin.readline().split()))
 M = int(input())
 targets = list(map(int, sys.stdin.readline().split()))
 
+A.sort() 
+
 for target in targets:
-    if target in A:
-        print(1)
+    start = 0
+    end = N-1
+    
+    while start <= end:
+        mid = (start + end) // 2
+
+        if target == A[mid]:
+            print(1)
+            break
+        elif A[mid] < target:
+            start = mid + 1
+        else:
+            end = mid -1
     else:
         print(0)
