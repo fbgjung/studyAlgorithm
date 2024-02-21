010a2180508eea9f22ea9ba0587315384a89136b
jjungeeu <fbgjung@gmail.com>
2024-02-21 19:45:25 +0900

    #11  [백준][실버4][자료구조] 나는야 포켓몬 마스터 이다솜

--- baekjoon/class3/나는야포켓몬마스터이다솜.py
+++ baekjoon/class3/나는야포켓몬마스터이다솜.py
@@ -1,16 +1,35 @@
+# N, M = map(int, input().split()) 
+
+# pocketmons = []
+# questions = []
+
+# for _ in range(N):
+#     pocketmons.append(input())
+
+# for _ in range(M):
+#     question = input()
+
+#     if question in pocketmons:
+#         print(pocketmons.index(question)+1)
+#     elif question not in pocketmons:
+#         question = int(question)
+#         print(pocketmons[question-1])
+
+import sys
+input = sys.stdin.readline
+
 N, M = map(int, input().split()) 
+pocketmons = {} 
 
-pocketmons = []
-questions = []
+for i in range(1, N+1):
+    pocketmon = input().rstrip()
+    pocketmons[i] = pocketmon
+    pocketmons[pocketmon] = i
 
-for _ in range(N):
-    pocketmons.append(input())
 
 for _ in range(M):
-    question = input()
-
-    if question in pocketmons:
-        print(pocketmons.index(question)+1)
-    elif question not in pocketmons:
-        question = int(question)
-        print(pocketmons[question-1])
+    question = input().rstrip()
+    if question.isdigit():
+        print(pocketmons[int(question)])
+    else:
+        print(pocketmons[question])
