cd65303a2b99edbaffb98705b6f6c9b99a1a87c9
jjungeeu <fbgjung@gmail.com>
2024-02-26 17:01:51 +0900

    other solution: [프로그래머스][Lv1][해시] 포켓몬

--- programmers/algorithm/해시/포켓몬.py
+++ programmers/algorithm/해시/포켓몬.py
@@ -1,10 +1,15 @@
-def solution(nums):
-    answer = 0
-    choice = len(nums) // 2 
-    set_nums = list(set(nums))
+# def solution(nums):
+#     answer = 0
+#     choice = len(nums) // 2 
+#     set_nums = list(set(nums))
 
-    if len(set_nums) >= choice:
-        answer = choice
-    else:
-        answer = len(set_nums)
-    return answer
+#     if len(set_nums) >= choice:
+#         answer = choice
+#     else:
+#         answer = len(set_nums)
+#     return answer
+
+
+# other solution
+def solution(nums):
+    return min(len(set(nums)), len(nums)//2)
