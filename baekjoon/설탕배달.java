fec2e6f60702196e5d1dc1b16642e38270796f24
fbgjung <fbgjung@gmail.com>
2024-05-01 13:27:26 +0900

    java: [백준][실버4][그리디] 설탕 배달

---
+++ baekjoon/설탕배달.java
@@ -0,0 +1,27 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class 설탕배달 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int N = scanner.nextInt();
+        int result = 0;
+        
+
+        while (N > 0){
+            if (N % 5 == 0) {
+                result += N/5;
+                N = N % 5;
+            } else {
+                N -= 3;
+                result += 1;
+            }  
+            if (N < 0) {
+                result = -1;
+            }
+        }
+        System.out.println(result);
+        scanner.close();
+    } 
+}
