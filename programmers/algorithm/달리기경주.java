c676a609d646b497b84015179ec57279d0bd82d3
fbgjung <fbgjung@gmail.com>
2024-05-01 15:12:56 +0900

    [프로그래머스][Lv1][구현] 달리기 경주

---
+++ programmers/algorithm/달리기경주.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+class Solution {
+    public String[] solution(String[] players, String[] callings) {
+        
+        int num = players.length;
+                
+        Map<String, Integer> ranking = new HashMap<>();
+        for (int i = 0; i<num; i++) {
+            ranking.put(players[i], i);
+        }
+        
+        for (String c : callings) {
+            int playerRanking = ranking.get(c); // 호출된 선수의 현재 랭킹
+            
+            String frontPlayer = players[playerRanking-1]; // 앞 선수의 이름
+            
+            players[playerRanking] = frontPlayer;
+            players[playerRanking-1] = c;
+            
+            ranking.put(c, playerRanking-1);
+            ranking.put(frontPlayer, playerRanking);
+        }
+        return players;
+    }
+}
