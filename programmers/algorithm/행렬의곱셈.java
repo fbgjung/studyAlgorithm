9782665c63e589e25b8497c9f23a21e059861e83
fbgjung <fbgjung@gmail.com>
2024-05-04 20:38:02 +0900

    [프로그래머스][Lv2][구현] 행렬의 곱셈

---
+++ programmers/algorithm/행렬의곱셈.java
@@ -0,0 +1,18 @@
+class 행렬의곱셈 {
+    public int[][] solution(int[][] arr1, int[][] arr2) {
+        int m = arr1.length;
+        int n = arr2[0].length;
+        int[][] answer = new int[m][n];
+        System.out.println(answer);
+
+        for (int i = 0; i < m; i++){
+            for (int j = 0; j < n; j++){
+                for (int k = 0; k < arr1[0].length; k++){
+                    answer[i][j] += arr1[i][k] * arr2[k][j];
+                }
+            }
+        }
+        
+        return answer;
+    }
+}
