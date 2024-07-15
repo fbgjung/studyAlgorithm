afcd362a2c0e9cc097dd45cf96c55a560c381216
fbgjung <fbgjung@gmail.com>
2024-07-15 23:19:18 +0900

    fail: #36 [백준][실버2][BFS] 헌내기는 친구가 필요해
    - 시간초과

---
+++ baekjoon/java/헌내기는친구가필요해.java
@@ -0,0 +1,55 @@
+package baekjoon.java;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.StringTokenizer;
+import java.util.Queue;
+
+public class 헌내기는친구가필요해 {
+    
+    static int n,m,result;
+    static char[][] campus;
+    static int[] dx = {-1,1,0,0};
+    static int[] dy = {0,0,-1,1};
+    static Queue<int[]> queue = new ArrayDeque<>();
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        campus = new char[n][m];
+
+        for (int i = 0; i < n; i++) {
+            String str = br.readLine();
+            campus[i] = str.toCharArray();
+            for (int j = 0; j < m; j++) {
+                if (campus[i][j] == 'I') queue.offer(new int[]{i,j});
+            }
+        }
+
+        while(!queue.isEmpty()) {
+            int current[] = queue.poll();
+            int x = current[0];
+            int y = current[1];
+
+            for (int i = 0; i < 4; i++) {
+                int nx = x + dx[i];
+                int ny = y + dy[i];
+
+                if (0 <= nx && nx < n && 0 <= ny && ny < m) {
+                    if (campus[nx][ny] == 'O') queue.offer(new int[]{nx,ny});
+                    if (campus[nx][ny] == 'P') {
+                        campus[nx][ny] = 'O';
+                        queue.offer(new int[]{nx,ny});
+                        result += 1;
+                    }
+                }
+            }
+        }
+        // 결과 출력 
+        if (result == 0) System.out.println("TT");
+        else System.out.println(result);
+    }
+}
